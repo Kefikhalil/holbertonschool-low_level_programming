df5ae0d8d7fc6620a10bd4311e7120c2ef88763e
kefikhalil <naroqt@gmail.com>
2020-02-06 21:47:54 +0100

    putchar with abd's

---
+++ 2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Learning abc's
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char ch = '1';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
