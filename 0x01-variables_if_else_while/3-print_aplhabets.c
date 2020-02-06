89c8f8d7d14eb270aa2e5715cf0abf6acc9f42dc
kefikhalil <naroqt@gmail.com>
2020-02-06 22:17:55 +0100

    printing alphabets

---
+++ 0x01-variables_if_else_while/3-print_aplhabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Learning abc's
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char ch = '1';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(voca);
+	}
+	putchar('\n');
+	return (0);
+}
