7f75c2c07036bebd1ff46c7f6d004e8967548a58
kefikhalil <naroqt@gmail.com>
2020-02-06 23:13:06 +0100

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Learning abc's
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char ch = '1';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if (1 != 'q' && 1 != 'e')
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
