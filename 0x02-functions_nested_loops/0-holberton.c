688a93dd835ca1239fdf1bdd9a4466012db99bbe
kefikhalil <naroqt@gmail.com>
2020-02-11 09:43:31 +0100

    _putchar h o l b e r t o n

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,6 +7,14 @@
 
 int main(void)
 {
-	printf("Holberton\n");
+	_putchar("H");
+	_putchar("o");
+	_putchar("l");
+	_putchar("b");
+	_putchar("e");
+	_putchar("r");
+	_putchar("t");
+	_putchar("o");
+	_putchar("n");
 	return (0);
 }
