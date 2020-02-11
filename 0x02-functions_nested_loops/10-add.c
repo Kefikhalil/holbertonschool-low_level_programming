be3ddaf33ce3a6b28eb456443ad6e6631b854707
kefikhalil <naroqt@gmail.com>
2020-02-11 22:57:20 +0100

    add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - A function that adds two integers
+ *@x: number of the sum operation
+ *@y: number of the sum operation
+ *Return: res: the result of the sum.
+ **/
+int add(int x, int y)
+{
+return (x + y);
+}
