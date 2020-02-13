553abaf49fa48a940bdf319bb3a091ab1497d6cf
Kefikhalil <naroqt@gmail.com>
2020-02-13 14:26:30 +0000

    print from 0-9 without 2 & 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,7 @@ void print_most_numbers(void)
 int n;
 for (n = 0; n <= 9; n++)
 {
-if (n == 2 || n == 4)
-{
-n++;
-continue;
-}
-_putchar(n++);
-}
+if (n != 2 && n !=4)
+_putchar(n + '0');
 _putchar('\n');
 }
