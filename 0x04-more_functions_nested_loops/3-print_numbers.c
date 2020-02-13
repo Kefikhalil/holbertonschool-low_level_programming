f4425524150f5acf7dbe25bfcf1e89d5edcf229a
Kefikhalil <naroqt@gmail.com>
2020-02-13 14:01:50 +0000

    print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_numbers - print number from 0 to 9
+ *
+ * Return: 0-9
+ */
+
+void print_numbers(void)
+{
+int ch;
+for (ch = 0; ch = 9, ch++)
+{
+_putchar (ch + '0');
+}
+_putchar ('\n');
+}
