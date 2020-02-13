c2e01f1b4d02f8d7b61bea9bf797b241a46f65b0
Kefikhalil <naroqt@gmail.com>
2020-02-13 21:09:41 +0000

    largest factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out the larget prime factor
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+long int num = 612852475143;
+long int i = 2;
+long int largest;
+
+while (num != 0)
+{
+if (num % i != 0)
+{
+i += 1;	
+{ 
+else
+{
+largest = num;
+num /= i;
+if (num == 1)
+{
+printf("%ld\n", largest);
+break;
+}
+}
+}
+return (0);
+}
