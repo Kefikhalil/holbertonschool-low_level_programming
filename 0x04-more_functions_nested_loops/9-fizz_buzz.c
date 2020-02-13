1ca5943a6a2f88ee096609108281adef0de1e601
Kefikhalil <naroqt@gmail.com>
2020-02-13 20:59:52 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,24 +4,23 @@
  *
  * Return: 0
  */
-
 int main(void)
 {
 int i;
 
 for (i = 1; i <= 100; i++)
 {
-if (i % 15 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 printf("FizzBuzz");
 else if (i % 3 == 0)
 printf("Fizz");
 else if (i % 5 == 0)
 printf("Buzz");
 else
-printf("%i", i);
-if (i < 100)
-printf(" ");
+printf("%d", i);
+if (i != 100)
+putchar(' ');
 }
-printf("\n");
+putchar('\n');
 return (0);
 }
