1b999c8226ee80d767d1303345bef0d8c98d45de
Kefikhalil <naroqt@gmail.com>
2020-02-18 22:22:27 +0000

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+/**
+ * puts2 - prints every other character
+ * @str:  pointer
+ * Return: 0
+ */
+void puts2(char *str)
+{
+int a;
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a % 2 == 0)
+_putchar(str[a]);
+}
+_putchar('\n');
+}                                                                                                   ~       
