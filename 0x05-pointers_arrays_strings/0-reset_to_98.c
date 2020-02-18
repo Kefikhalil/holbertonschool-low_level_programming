78940195147edc47099902f578bdac83ec8e2014
Kefikhalil <naroqt@gmail.com>
2020-02-18 10:51:34 +0000

    pointers , arrays and strings.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - reset_to_98 code that point 98
+ *
+ *@n: n
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+int n;
+int *ip = 98;
+
+printf("%d\n", &*ip);
+return (0);
+}
