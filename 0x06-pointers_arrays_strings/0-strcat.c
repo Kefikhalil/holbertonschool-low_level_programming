4b1cfd1e7937eacbb0ce13ba9b28c02a29b73b4c
Kefikhalil <naroqt@gmail.com>
2020-02-20 21:42:38 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,14 @@
  *
  * Return: 0
  */
-char *_strcat(char *dest, char *src)
+char *strcat(char *dest, const char *src)
 {
-int i, j;
+int index = 0, dest_len = 0;
 
-i = j = 0;
-while (*(dest + i))
-i++;
-while ((*(dest + i) = *(src + j)))
-{
-i++;
-j++;
-}
+while (dest[index++])
+dest_len++;
+
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
+
 return (dest);
-}
