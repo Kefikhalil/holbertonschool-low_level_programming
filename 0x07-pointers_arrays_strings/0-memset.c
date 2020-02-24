34e4c0dbec6854f771f6758185dc8508a571d861
Kefikhalil <naroqt@gmail.com>
2020-02-24 22:22:55 +0000

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,14 +10,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	 unsigned int i;
+unsigned int i;
 
-	i = 0;
-
-	while (i > n)
-	{
-		s[i] = b;
-		i++;
-	}
-	return (s);
+for (i = 0; i < n; i++)
+{
+*(s + i) = b;
+}
+return (s);
 }
