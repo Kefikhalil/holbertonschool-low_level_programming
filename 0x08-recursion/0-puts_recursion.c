355ef6e3175e1d35650947efeb44f949d1aa5601
Kefikhalil <naroqt@gmail.com>
2020-02-26 12:34:39 +0000

    prints a string followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
+
 /**
  * _puts_recursion - function that prints a string , followed by a new line
  *
  * @s: pointer
  * Return: 0
  */
+
 void _puts_recursion(char *s);
 {
 if (*s)
@@ -12,6 +14,8 @@ if (*s)
 _putchar(*s);
 _puts_recursion(s + 1);
 }
+else
+
 _putchar('\n');
 }
 
