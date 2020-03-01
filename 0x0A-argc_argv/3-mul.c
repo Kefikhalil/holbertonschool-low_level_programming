3a75fdb9646a8a799a239149702765b59e200925
Kefikhalil <naroqt@gmail.com>
2020-03-01 22:08:28 +0000

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,20 +15,4 @@ int sum, val, i;
 sum = 0;
 if (argc < 1)
 printf("%d\n", 0);
-while (argc-- && argc > 0)
-{
-for (i = 0; argv[argc][i] != '\0'; i++)
-{
-
-if (!(isdigit(argv[argc][i])))
-{
-printf("Error\n");
-return (1);
-}
-}
-val = atoi(argv[argc]);
-sum += val;
-}
-printf("%d\n", sum);
-return (0);
 }
