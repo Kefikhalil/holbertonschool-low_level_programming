078943db538153fc0fdd5d749cac7ad1a7e9131c
Kefikhalil <naroqt@gmail.com>
2020-03-10 21:13:03 +0000

    free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,19 +1,3 @@
 #include "dog.h"
 #include <stdlib.h>
 
-/**
- *free_dog - free memorty already allocated
- *@d: pointer
- *
- *Return: 0
-*/
-
-void free_dog(dog_t *d)
-{
-if (d)
-{
-free(d ->name);
-free(d ->owner);
-free(d);
-}
-}
