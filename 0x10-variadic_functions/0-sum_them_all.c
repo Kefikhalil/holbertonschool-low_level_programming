824576fc7cd58613484a340b9e14820f42d8680a
Kefikhalil <naroqt@gmail.com>
2020-03-18 19:45:35 +0000

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,10 +12,15 @@ int sum_them_all(const unsigned int n, ...)
 
 va_list ap;
 int sum = 0, i;
+va_list ap;
+
+if (n == 0)
+return (0);
 va_start(ap, n);
 
 for (i = 0 ; i < n ; i++)
 sum += va_arg(ap, int);
+
 va_end(ap);
 return (sum);
 }
