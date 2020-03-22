786da185a58abbe9aafe5293cac734044aaf81ba
Kefikhalil <naroqt@gmail.com>
2020-03-22 22:25:38 +0000

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: haed
+ *
+ * Return: 0
+ */
+
+void free_list(list_t *head)
+{
+
+list_t *tmp;
+
+while (head != NULL)
+{
+tmp = head;
+head = head->next;
+free(tmp->str);
+free(tmp);
+}
+}
