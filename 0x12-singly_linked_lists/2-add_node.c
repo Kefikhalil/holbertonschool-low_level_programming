394e641a5ea27ba48c03629a710bbd495748a156
Kefikhalil <naroqt@gmail.com>
2020-03-22 22:22:07 +0000

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: head of node
+ * @str: string
+ *
+ * Return: 0
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+int _strlen(const char *s)
+{
+int i;
+
+for (i = 0; s[i] != 0; i++)	
+{
+}
+return (i);
+}
+
