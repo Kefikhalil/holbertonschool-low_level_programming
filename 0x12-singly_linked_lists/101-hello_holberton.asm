fa23ca2a2861f8733c7fbb06b93b888961cec90e
Kefikhalil <naroqt@gmail.com>
2020-03-23 23:06:41 +0000

    hello Holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+section .data
+
+format:
+msg db	"Hello, Holberton", 0
+fmt db	"%s", 10, 0
+
+section .text
+extern printf
+global main
+
+main:
+
+mov 	esi, msg
+
+mov 	edi, fmt
+
+mov	eax, 0
+
+call	printf
+
+mov	ebx, 0
