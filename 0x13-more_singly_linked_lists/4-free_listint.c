f4b477d017284e97c5c5b0e878734944c47fcf49
Kefikhalil <naroqt@gmail.com>
2020-03-27 15:42:42 +0000

    frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list
+ * @head: list
+ */
+
+void free_listint(listint_t *head)
+{
+while (head != NULL)
+{
+free(head);
+head = head->next;
+}
